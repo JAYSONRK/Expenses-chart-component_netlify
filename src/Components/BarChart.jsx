001f3670030ea8782ac5327f1fa2c8cdbe2171e1
JAYSONRK <145671328+JAYSONRK@users.noreply.github.com>
2023-12-02 23:58:12 +0530

    Update BarChart.jsx

--- src/Components/BarChart.jsx
+++ src/Components/BarChart.jsx
@@ -3,6 +3,7 @@ import { Bar } from 'react-chartjs-2';
 import {Chart as ChartJS} from 'chart.js/auto'; 
 
 const BarChart = (prop) => {
+    console.log(ChartJS);
     const options = {
         plugins: {
           legend: {
